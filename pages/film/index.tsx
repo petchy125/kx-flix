f32107cc760c05c1a76f372cb71d7b1b918b692d
Amornwut Lertsiriumnouyphon <amornwut.l@kbtg.tech>
2023-08-30 10:31:43 +0700

    just convert all js to tsx

--- pages/film/index.tsx
+++ pages/film/index.tsx
@@ -1,9 +1,5 @@
 import React, { FC } from "react";
-
 import dynamic from "next/dynamic";
-
 const HomePage = dynamic(() => import("@Containers/HomePage"));
-
 const App: FC = () => <HomePage />;
-
 export default App;
